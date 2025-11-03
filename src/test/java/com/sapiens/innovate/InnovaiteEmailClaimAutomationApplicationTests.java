a62e89c150226a6bb6e7c1cacd342dedd2f56988
santoshmirashe <47006019+santoshmirashe@users.noreply.github.com>
2025-11-03 12:40:22 +0530

    initil commit
    initil commit

---
+++ src/test/java/com/sapiens/innovate/InnovaiteEmailClaimAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapiens.innovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaiteEmailClaimAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
