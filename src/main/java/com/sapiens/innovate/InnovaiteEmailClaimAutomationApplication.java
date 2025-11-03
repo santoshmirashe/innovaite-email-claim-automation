9c315e1edc24e8395cc09a75c0cf409de91eea6a
santoshmirashe <47006019+santoshmirashe@users.noreply.github.com>
2025-11-03 14:02:15 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/com/sapiens/innovate/InnovaiteEmailClaimAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.sapiens.innovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaiteEmailClaimAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaiteEmailClaimAutomationApplication.class, args);
+	}
+
+}
