ad3e646a35b63f95421f754f8c01bb7416853f19
SantoshMirashe <mirashe.vishwasrao@sapiens.com>
2025-11-03 16:03:04 +0530

    Commit

--- src/main/java/com/sapiens/innovate/service/impl/ClaimServiceImpl.java
+++ src/main/java/com/sapiens/innovate/service/impl/ClaimServiceImpl.java
@@ -3,6 +3,9 @@ package com.sapiens.innovate.service.impl;
 import com.sapiens.innovate.service.inf.ClaimService;
 import com.sapiens.innovate.service.inf.EmailService;
 import com.sapiens.innovate.vo.ClaimDataVO;
+import com.sapiens.innovate.vo.EmailVO;
+
+import java.util.List;
 
 public class ClaimServiceImpl implements ClaimService {
 
@@ -21,6 +24,19 @@ public class ClaimServiceImpl implements ClaimService {
 
     @Override
     public String processClaims() {
+        EmailServiceImpl emailService = (EmailServiceImpl) EmailServiceImpl.getInstance();
+        GPTProcessorImpl gptProcessor = (GPTProcessorImpl) GPTProcessorImpl.getInstance();
+        List<EmailVO>  mails = emailService.getAllEmails();
+        mails.forEach(mail ->{
+            try {
+                ClaimDataVO claimDataVO = gptProcessor.analyzeMessage(mail);
+                this.raiseClaim(claimDataVO);
+                emailService.markMessageRead(mail.getMessageID());
+                //Add send email logic
+            } catch (Exception e) {
+                System.out.println("Error : "+e.getMessage());
+            }
+        });
         return null;
     }
 }
--- src/main/java/com/sapiens/innovate/service/impl/GPTProcessorImpl.java
+++ src/main/java/com/sapiens/innovate/service/impl/GPTProcessorImpl.java
@@ -1,10 +1,19 @@
 package com.sapiens.innovate.service.impl;
 
+import com.sapiens.innovate.service.inf.EmailService;
 import com.sapiens.innovate.service.inf.GPTProcessor;
 import com.sapiens.innovate.vo.ClaimDataVO;
 import com.sapiens.innovate.vo.EmailVO;
 
 public class GPTProcessorImpl implements GPTProcessor {
+    private static GPTProcessor instance;
+
+    public static synchronized GPTProcessor getInstance() {
+        if (instance == null) {
+            instance = new GPTProcessorImpl();
+        }
+        return instance;
+    }
 
     @Override
     public ClaimDataVO analyzeMessage(EmailVO message) throws Exception {
